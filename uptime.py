1d54f04468215f20baf4c0487964a1dd9d91c728
Tom Mitchell <tom@tom.org>
2017-12-10 15:00:36 -0500

    point to other web, generalize bin

--- uptime.py
+++ uptime.py
@@ -3,40 +3,42 @@ import logging
 import time
 import urllib2
 
-URL = "http://web.tom.org:3000/vt/"
+URL = "http://eero.com/"
 INTERVAL = 60
+AIRPORT_BIN = "/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport"
 
-logging.basicConfig(filename='./uptime.log', level=logging.INFO, \
+logging.basicConfig(filename='./uptime.log', level=logging.DEBUG, \
                   format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
 
 def logit():
-    logging.info(subprocess.check_output(['/Users/tom/bin/airport', '-s']))
-    logging.info(subprocess.check_output(['/Users/tom/bin/airport', '-I']))
+    logging.debug("logit()")
+    logging.info(subprocess.check_output([AIRPORT_BIN, '-s']))
+    logging.info(subprocess.check_output([AIRPORT_BIN, '-I']))
+
+online = True
+went_offline = came_online = time.localtime()
+tries = 0
 
 logging.info("starting up monitoring")
 logit()
 
-
 while True:
-    online = True
-    went_offline = came_online = time.localtime()
-    tries = 0
     try:
         tries += 1
         urllib2.urlopen(URL, timeout=5).read()
         if not online:
             online = True
             came_online = time.localtime()
-            logging.info("came back online")
-            logging.info("offline since: %s", time.asctime(went_offline))
+            logging.warning("came back online")
+            logging.warning("offline since: %s", time.asctime(went_offline))
             logit()
         if not tries % INTERVAL:
             logging.info("Checkpointing: ")
             logit()
             logging.info("Done checkpointing: ")
-        logging.debug("ok: %s", time.asctime())
+        logging.debug("retrieval ok: %d", tries)
     except Exception as e:
-        logging.info("not ok")
+        logging.error("not ok")
         print "not ok"
         logit()
         if online:
