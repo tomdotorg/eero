1c15935d09b6949c1f3f200b5b7ac8a8eef931ce
Tom Mitchell <tom@tom.org>
2017-12-11 06:27:43 -0500

    switch to ping

--- uptime.py
+++ uptime.py
@@ -1,13 +1,15 @@
+import os
 import subprocess
 import logging
 import time
 import urllib2
 
 URL = "http://192.168.1.4/stats.htm"
+HOST = "192.168.1.254"
 INTERVAL = 60
 AIRPORT_BIN = "/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport"
 
-logging.basicConfig(filename='./uptime.log', level=logging.INFO, \
+logging.basicConfig(filename='./uptime.log', level=logging.DEBUG, \
                   format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
 
 def logit():
@@ -22,10 +24,28 @@ tries = 0
 logging.info("starting up monitoring")
 logit()
 
+
+def check_http(url):
+    logging.debug("check_http()")
+    try:
+        urllib2.urlopen(url, timeout=5).read()
+        return True
+    except Exception as e:
+        raise e
+
+
+def check_ping(hostname):
+    logging.debug("check_ping()")
+    response = os.system("ping -c 1 -W1 " + hostname + " > /dev/null 2>&1")
+    if response == 0:
+        return
+    else:
+        raise Exception('ping failed')
+
 while True:
     try:
         tries += 1
-        urllib2.urlopen(URL, timeout=5).read()
+        check_ping(HOST)
         if not online:
             online = True
             came_online = time.localtime()
